83345b5b9ec5506e78facd1d5b40c1c277281301
alexis pondo <alexispondo23@gmail.com>
2021-04-08 11:04:47 +0000

    second code

--- TP1/site/csvExpor.php
+++
@@ -1,4 +0,0 @@
-
-<form method="POST" action="csvExpor1.php">
-                             <input class="btn btn-warning" type="submit" name="export" value="exporter la base" />
-                                </form>
